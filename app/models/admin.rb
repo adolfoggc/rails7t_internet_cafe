64d5da5d48265f808b1800f782dc7a8bc8d48cc9
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-12-19 10:15:09 -0300

    Create Admin

---
+++ app/models/admin.rb
@@ -0,0 +1,11 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum kind: {
+    employee: 0,
+    supervisor: 1
+  }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  devise_for :admins
   root 'site#index'
   mount Sidekiq::Web => '/sidekiq'
 
